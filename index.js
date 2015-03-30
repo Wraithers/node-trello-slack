4f81da0bed013e0baa9d2dee7e75a5941e8fe4a7
Aaron Khare <ask@aaronkhare.com>
2015-03-31 01:57:51 +0530

    revise structure

--- index.js
+++ index.js
@@ -8,21 +8,21 @@ var Bot = require('n-trello-slack'),
 		trello: {
 			boards: [
 				{
-					id: '7lo9Rm73',
-					channel: '#bnf'
+					id: '...',
+					channel: '#dev'
         		}
-//        		, {
-//					id: '...',
-//					channel: '#devops'
-  //      		}
+        		, {
+					id: '...',
+					channel: '#devops'
+      		}
     		],
-			key: 'b97f22ec035a407a41b24eb6a9c14f08',
-			token: 'c2f29f46a7f67fab0e7b9f6723175320a82b7283c9caf92dd6a0ce7e01f315b2'
+			key: '...',
+			token: '...'
 		},
 		slack: {
-			domain: '22feet',
-			token: 'test12345',
-			webhook: 'https://hooks.slack.com/services/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',
+			domain: '...',
+			token: '...',
+			webhook: '...',
 			channel: '#updates'
 		}
 	});
