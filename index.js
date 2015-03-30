73a9ef8f10746f3b6d74f7b2c3f7a511ab05afa3
Aaron Khare <ask@aaronkhare.com>
2015-03-30 18:44:23 +0530

    first build

---
+++ index.js
@@ -0,0 +1,28 @@
+#!/usr/bin/env node
+
+var Bot = require('node-trello-slack-ak'),
+	bot = new Bot({
+		pollFrequency: 1000 * 60 * 3 //every 3 minutes
+			,
+		start: true,
+		trello: {
+			boards: [
+				{
+					id: '7lo9Rm73',
+					channel: '#bnf'
+        		}
+//        		, {
+//					id: '...',
+//					channel: '#devops'
+  //      		}
+    		],
+			key: 'b97f22ec035a407a41b24eb6a9c14f08',
+			token: 'c2f29f46a7f67fab0e7b9f6723175320a82b7283c9caf92dd6a0ce7e01f315b2'
+		},
+		slack: {
+			domain: '22feet',
+			token: 'test12345',
+			webhook: 'https://hooks.slack.com/services/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',
+			channel: '#updates'
+		}
+	});
