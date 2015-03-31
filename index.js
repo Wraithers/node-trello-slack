6f003c01bebaaed86f2a2a792782bf18521ac11f
Aaron Khare <ask@aaronkhare.com>
2015-03-31 13:30:04 +0530

    added jr-social board

--- index.js
+++ index.js
@@ -10,11 +10,15 @@ var Bot = require('n-trello-slack'),
 				{
 					id: '7lo9Rm73',
 					channel: '#bnf'
-        		}
-        		, {
+        		},
+				{
 					id: 'lxviQlfu',
 					channel: '#jr-monthlytracker'
-        		}
+        		},
+				{
+					id: 'yMItwCDu',
+					channel: '#jr-social'
+				}
     		],
 			key: 'b97f22ec035a407a41b24eb6a9c14f08',
 			token: 'c2f29f46a7f67fab0e7b9f6723175320a82b7283c9caf92dd6a0ce7e01f315b2'
