f222ef6e14a87e6b931de15d75c1f92490466f3e
Aaron Khare <ask@aaronkhare.com>
2015-03-31 12:54:16 +0530

    added jr-monthly tracker board

--- index.js
+++ index.js
@@ -8,21 +8,21 @@ var Bot = require('n-trello-slack'),
 		trello: {
 			boards: [
 				{
-					id: '...',
-					channel: '#dev'
+					id: '7lo9Rm73',
+					channel: '#bnf'
         		}
         		, {
-					id: '...',
-					channel: '#devops'
-      		}
+					id: 'lxviQlfu',
+					channel: '#jr-monthlytracker'
+        		}
     		],
-			key: '...',
-			token: '...'
+			key: 'b97f22ec035a407a41b24eb6a9c14f08',
+			token: 'c2f29f46a7f67fab0e7b9f6723175320a82b7283c9caf92dd6a0ce7e01f315b2'
 		},
 		slack: {
-			domain: '...',
-			token: '...',
-			webhook: '...',
+			domain: '22feet',
+			token: 'test12345',
+			webhook: 'https://hooks.slack.com/services/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',
 			channel: '#updates'
 		}
 	});
